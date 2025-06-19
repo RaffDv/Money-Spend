000df8946113d08203adee024fd868871dcfc31d
Rafael Vargas <rafael.vargas.emp@gmail.com>
2025-06-19 20:50:12 -0300

    fix: ecossystem

--- apps/web/ecosystem.config.cjs
+++ apps/web/ecosystem.config.cjs
@@ -1,20 +1,23 @@
 module.exports = {
-  apps: [{
-    script: 'pnpm start',
-  },],
+	apps: [
+		{
+			script: "pnpm start",
+		},
+	],
 
-  deploy: {
-    production: {
-      key: 'github_servers',
-      user: 'ubuntu',
-      host: '159.112.189.45',
-      ref: 'origin/prod',
-      repo: 'git@github.com:RaffDv/Money-Spend.git',
-      path: '/home/ubuntu/',
-      'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh && pnpm install && pnpm build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes'
-    }
-  }
+	deploy: {
+		production: {
+			key: "github_servers",
+			user: "ubuntu",
+			host: "159.112.189.45",
+			ref: "origin/prod",
+			repo: "git@github.com:RaffDv/Money-Spend.git",
+			path: "/home/ubuntu/",
+			"pre-deploy-local": "",
+			"post-deploy":
+				"source ~/.nvm/nvm.sh && pnpm install && pnpm build && pm2 reload ecosystem.config.js ",
+			"pre-setup": "",
+			ssh_options: "ForwardAgent=yes",
+		},
+	},
 };
