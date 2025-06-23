3c11404149a2e8126f877ab004f3b8c2186d8db3
Rafael Vargas <rafael.vargas.emp@gmail.com>
2025-06-23 18:54:29 -0300

    feat(web): wip | add a initial landing page

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
-
-const Page = () => {
-	return <span>Intinal page</span>;
-};
-
-export default Page;
