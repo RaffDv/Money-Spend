f7371fc355f52ae220b1346c71fdd99697da0f00
Rafael Vargas <rafael.vargas.emp@gmail.com>
2025-08-01 20:51:35 -0300

    fix: redirect google login

--- apps/web/app/auth/callback/route.ts
+++ apps/web/app/auth/callback/route.ts
@@ -26,7 +26,7 @@ export async function GET(request: Request) {
 			const isLocalEnv = process.env.NODE_ENV === "development";
 			if (isLocalEnv) {
 				// we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-				return NextResponse.redirect(`http://localhost:3000${next}`);
+				return NextResponse.redirect(`${origin}${next}`);
 			} else if (forwardedHost) {
 				return NextResponse.redirect(`https://${forwardedHost}${next}`);
 			} else {
